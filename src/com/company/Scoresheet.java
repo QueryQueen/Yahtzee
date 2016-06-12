46be0088a1e0307dd3de55b2243a66115369e356
QueryQueen <Joedid@gmail.com>
2016-06-12 19:45:18 +0200

    Final working version for now, checks for 3 options to calculate turn score (yahtzee, grote straat, chance)

--- src/com/company/Scoresheet.java
+++ src/com/company/Scoresheet.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -12,6 +13,8 @@ public class Scoresheet {
     //We need a scanner to get userInput
     Scanner sc = new Scanner(System.in);
 
+    ArrayList<Integer> yahtzeeCheck = new ArrayList<Integer>();
+
     //to get the result of 1 turn, the player needs to roll the dice max 3 times
     //after each roll you can decide which dice you want to hold
     int getTurnResult(Die[] dice) {
@@ -20,16 +23,18 @@ public class Scoresheet {
         int score;
 
         //in each turn you are allowed to roll the dice 3 times
-        for(int i = 0; i < 3; i++){
+        for(int i = 0; i < 3; i++) {
             rollDiceTurn(dice);
             result = getDiceValues(dice);
-            System.out.println("You rolled" + Arrays.toString(result));
-            holdDice(dice);
             rollCount++;
             System.out.println("you rolled the dice " + rollCount + " times");
+            System.out.println("You rolled" + Arrays.toString(result));
+            if (rollCount < 3) {
+                holdDice(dice);
+            }
         }
         rollCount = 0;
-        System.out.println("The result of your turn is" + Arrays.toString(result));
+        System.out.println("The result of your turn is" + Arrays.toString(getDiceValues(dice)));
         freeDice(dice);
         score = calcTurnScore(result);
         return score;
@@ -76,14 +81,59 @@ public class Scoresheet {
     //for now, the score of 1 turn is calculated by summing all dices
     //later we can implement the Yahtzee rules for official scores
     int calcTurnScore(int[] diceResult) {
-        int sum = 0;
+        int score = 0;
 
-        for (int i : diceResult) {
-            sum += i;
+        // create an ArrayList so I can check for real Yahtzee scores
+        for(int i = 0; i < diceResult.length; i++){
+            yahtzeeCheck.add(diceResult[i]);
         }
-        return sum;
+
+        boolean largeStraight = largeStraight(yahtzeeCheck);
+        boolean yahtzee = yahtzee(yahtzeeCheck);
+
+        if(largeStraight){
+            System.out.println("Category: large straigth");
+            score = 40;
+        } else if(yahtzee){
+            System.out.println("Category: yahtzee!");
+            score = 50;
+        } else {
+            for (int i : diceResult) {
+                score += i;
+                }
+            System.out.println("Category: chance");
+            }
+        return score;
+        }
+
+    //the sole purpose of this method is to check for Yahtzee
+    boolean yahtzee(ArrayList<Integer> yahtzeeCheck){
+        boolean yahtzee = true;
+        //I need to check if all the dice have the same value
+        for(int i = 0; i < yahtzeeCheck.size()-1; i++){
+            if(!(yahtzeeCheck.get(i).equals(yahtzeeCheck.get(i+1)))){
+                yahtzee = false;
+            }
+        }
+        return yahtzee;
     }
+
+    //the sole purpose of this method is to check for large straight
+    boolean largeStraight(ArrayList<Integer> yahtzeeCheck) {
+        boolean largeStraight = false;
+        //I need to check if each value 1 - 5 or 2 - 6 are present in the arraylist
+
+        if (yahtzeeCheck.indexOf(1) != -1 && yahtzeeCheck.indexOf(2)!= -1 && yahtzeeCheck.indexOf(3) != -1 && yahtzeeCheck.indexOf(4) != -1 && yahtzeeCheck.indexOf(5) != -1 ) {
+            largeStraight = true;
+             System.out.println(yahtzeeCheck.indexOf(1));
+        } else if(yahtzeeCheck.indexOf(2) != -1 && yahtzeeCheck.indexOf(3)!= -1 && yahtzeeCheck.indexOf(4) != -1 && yahtzeeCheck.indexOf(5) != -1 &&  yahtzeeCheck.indexOf(6) != -1){
+            largeStraight = true;
+        }
+        return largeStraight;
+    }
+
 }
+
 
 
 
---
+++ tests/com/company/ScoresheetTest.java
@@ -0,0 +1,49 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Joedid on 12/06/16.
+ */
+public class ScoresheetTest {
+    @org.junit.Test
+    public void yahtzeeFalse() throws Exception {
+        Scoresheet tested = new Scoresheet();
+        ArrayList<Integer> given = new ArrayList<Integer>(Arrays.asList(1,3,1,2,1));
+        assertEquals(false, tested.yahtzee(given));
+    }
+
+    @org.junit.Test
+    public void yahtzeeGood() throws Exception {
+        Scoresheet tested = new Scoresheet();
+        ArrayList<Integer> given = new ArrayList<Integer>(Arrays.asList(1,1,1,1,1));
+        assertEquals(true, tested.yahtzee(given));
+    }
+
+    @org.junit.Test
+    public void largeStraightGood1() throws Exception {
+        Scoresheet tested = new Scoresheet();
+        ArrayList<Integer> given = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5));
+        assertEquals(true, tested.largeStraight(given));
+    }
+
+    @org.junit.Test
+    public void largeStraightGood2() throws Exception {
+        Scoresheet tested = new Scoresheet();
+        ArrayList<Integer> given = new ArrayList<Integer>(Arrays.asList(2,3,4,5,6));
+        assertEquals(true, tested.largeStraight(given));
+    }
+
+
+    @org.junit.Test
+    public void largeStraightBad() throws Exception {
+        Scoresheet tested = new Scoresheet();
+        ArrayList<Integer> given = new ArrayList<Integer>(Arrays.asList(1,1,2,4,5));
+
+        assertEquals(false, tested.largeStraight(given));
+    }
+
+}
